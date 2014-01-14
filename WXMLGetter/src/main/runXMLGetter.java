8217c10fab33734062eb6d60c83af477f9918eff
Balzzanar <Balzzanar@users.noreply.github.com>
2014-01-14 08:03:35 -0800

    Initial commit

---
+++ WXMLGetter/src/main/runXMLGetter.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by wades on 1/14/14.
+ */
+public class runXMLGetter {
+    public static void main(String[] args) {
+        System.out.println("hejsan");
+    }
+}
